700f57d3a0c3560de33540114fff85685b1f3996
sripujithachitrala-sys <sripujithachitrala@gmail.com>
2026-08-20 08:58:50 +0000

    architecture

---
+++ test.py
@@ -0,0 +1,14 @@
+# arr=[1,2,3,4]
+# arr[2]=5
+# print(arr)
+# dic1={
+#     "name": "john",
+#     "age": 30,
+#     "city": "New York"
+# }
+# print(dict1["name"])
+# dict1["name"]="Pujitha"
+# print(dict1.get("name1","noname"))
+# x=2,3
+# print(x)
+
