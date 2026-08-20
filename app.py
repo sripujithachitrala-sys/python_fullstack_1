f82f5a3b988b90cb1d20657ab5c5ed4d3a4a33a0
sripujithachitrala-sys <sripujithachitrala@gmail.com>
2026-08-20 10:49:59 +0000

    html

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")  
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")   
+
+@app.route("/login")
+def login():
+    return render_template("login.html")       
+
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")                      
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
